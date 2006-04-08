f262d7a50aa2f1ee6a6c73d68dbd96526b0cf75e
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2006-04-08 19:10:47 +0000

    Creating a private branch based on autotools

---
+++ branches/autotools-branch/FreeMat2/tools/helpgen/helpgen.hpp
@@ -0,0 +1,17 @@
+#ifndef __helpgen_hpp__
+#define __helpgen_hpp__
+
+#include <QWidget>
+
+class ConsoleWidget : public QWidget {
+  Q_OBJECT
+private slots:
+  void LaunchHelpWindow();
+  void exitNow();
+  void Run();
+public:
+  ConsoleWidget();
+  virtual ~ConsoleWidget() {};
+};
+
+#endif
